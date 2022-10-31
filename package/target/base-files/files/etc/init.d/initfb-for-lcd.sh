bd1f37c0e69ca2feb1f8c53b906b716b0f8ab747
max_10 <max_10@gmx.de>
2022-10-31 14:26:58 +0100

    Revert "- E4HD 4K Ultra: fix add initfb for lcd init"
    This reverts commit 1c48535224ad83d2f2ce6c094fde4b63bc05f5aa.

--- package/target/base-files/files/etc/init.d/initfb-for-lcd.sh
+++ package/target/base-files/files/etc/init.d/initfb-for-lcd.sh
@@ -2,20 +2,10 @@
 # Licensed under GPLv2
 # initfb for lcd init
 
-### BEGIN INIT INFO
-# Provides:          initfb-for-lcd
-# Required-Start:
-# Required-Stop:
-# Default-Start:     S
-# Default-Stop:
-### END INIT INFO
-
 if [ -e /usr/bin/initfb ]; then
 	/usr/bin/initfb
 fi
 
-if [ -e /proc/stb/lcd/oled_brightness ]; then
-	echo 200 > /proc/stb/lcd/oled_brightness
-fi
+echo 200 > /proc/stb/lcd/oled_brightness
 
 exit 0
