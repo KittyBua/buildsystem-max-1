97ada8b6793c9b451de098b07b00e88f31f1afa0
max_10 <max_10@gmx.de>
2022-12-04 21:23:25 +0100

    - openvpn: bump version to 2.5.8

--- package/target/openvpn/openvpn.mk
+++ package/target/openvpn/openvpn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENVPN_VERSION = 2.5.7
+OPENVPN_VERSION = 2.5.8
 OPENVPN_DIR = openvpn-$(OPENVPN_VERSION)
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VERSION).tar.xz
 OPENVPN_SITE = http://build.openvpn.net/downloads/releases
@@ -32,7 +32,8 @@ OPENVPN_CONF_OPTS = \
 
 define OPENVPN_INSTALL_INIT_SYSV
 	mkdir -p $(TARGET_DIR)/etc/openvpn
-	$(INSTALL_EXEC) $(PKG_FILES_DIR)/openvpn $(TARGET_DIR)/etc/init.d/
+	$(INSTALL_EXEC) $(PKG_FILES_DIR)/openvpn $(TARGET_DIR)/etc/init.d/openvpn
+	$(UPDATE-RC.D) openvpn start 10 2 3 4 5 . stop 70 0 1 6 .
 endef
 
 $(D)/openvpn: | bootstrap
