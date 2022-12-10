8299a2d29e1f03408fad4a11206d1f1fcc607eef
max_10 <max_10@gmx.de>
2022-12-10 19:05:19 +0100

    - libxslt: bump version to 1.1.37

--- package/target/libxslt/libxslt.mk
+++ package/target/libxslt/libxslt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBXSLT_VERSION = 1.1.35
+LIBXSLT_VERSION = 1.1.37
 LIBXSLT_DIR = libxslt-$(LIBXSLT_VERSION)
 LIBXSLT_SOURCE = libxslt-$(LIBXSLT_VERSION).tar.xz
 LIBXSLT_SITE = https://download.gnome.org/sources/libxslt/1.1
@@ -13,23 +13,22 @@ LIBXSLT_DEPENDS = libxml2
 
 LIBXSLT_CONFIG_SCRIPTS = xslt-config
 
-LIBXSLT_AUTORECONF = YES
-
 LIBXSLT_CONF_OPTS = \
-	CPPFLAGS="$(CPPFLAGS) -I$(TARGET_INCLUDE_DIR)/libxml2" \
-	--with-html-dir=$(REMOVE_htmldir) \
-	--enable-shared \
-	--disable-static \
-	--with-gnu-ld \
-	--without-python \
-	--without-crypto \
-	--without-debug \
-	--without-mem-debug
+	-DBUILD_SHARED_LIBS=ON \
+	-DLIBXSLT_WITH_DEBUGGER=OFF \
+	-DLIBXSLT_WITH_CRYPTO=OFF \
+	-DLIBXSLT_WITH_MEM_DEBUG=OFF \
+	-DLIBXSLT_WITH_MODULES=OFF \
+	-DLIBXSLT_WITH_PROFILER=ON \
+	-DLIBXSLT_WITH_PYTHON=OFF \
+	-DLIBXSLT_WITH_TESTS=OFF \
+	-DLIBXSLT_WITH_THREADS=ON \
+	-DLIBXSLT_WITH_XSLT_DEBUG=OFF
 
 define LIBXSLT_TARGET_CLEANUP
-	rm -rf $(addprefix $(TARGET_LIB_DIR)/,cmake libxslt-plugins xsltConf.sh)
+	rm -rf $(addprefix $(TARGET_LIB_DIR)/,xsltConf.sh)
 endef
 LIBXSLT_TARGET_CLEANUP_HOOKS += LIBXSLT_TARGET_CLEANUP
 
 $(D)/libxslt: | bootstrap
-	$(call autotools-package)
+	$(call cmake-package)
