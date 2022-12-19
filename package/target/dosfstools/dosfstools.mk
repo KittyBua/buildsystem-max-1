d892a2abf2b0027c4add0fd695cef1eda6b3b312
max_10 <max_10@gmx.de>
2022-12-19 23:01:19 +0100

    - dosfstools: update build

--- package/target/dosfstools/dosfstools.mk
+++ package/target/dosfstools/dosfstools.mk
@@ -11,14 +11,11 @@ DOSFSTOOLS_SITE = https://github.com/dosfstools/dosfstools/releases/download/v$(
 
 DOSFSTOOLS_AUTORECONF = YES
 
-DOSFSTOOLS_CFLAGS = $(TARGET_CFLAGS) -D_GNU_SOURCE -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -fomit-frame-pointer
-
 DOSFSTOOLS_CONF_OPTS = \
 	--sbindir=$(base_sbindir) \
 	--docdir=$(REMOVE_docdir) \
 	--without-iconv \
-	--enable-compat-symlinks \
-	CFLAGS="$(DOSFSTOOLS_CFLAGS)"
+	--enable-compat-symlinks
 
 $(D)/dosfstools: | bootstrap
 	$(call autotools-package)
