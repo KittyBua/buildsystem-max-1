0ad5c986a6c5e9ffc59e3cdb11748cc1ed6c9cb8
max_10 <max_10@gmx.de>
2023-01-23 22:50:15 +0100

    - revert BS_PATH for build ccache

--- package/pkg-kernel-module.mk
+++ package/pkg-kernel-module.mk
@@ -8,7 +8,7 @@ LINUX_MAKE_ENV = \
 	$(HOST_MAKE_ENV)
 
 KERNEL_MAKE_VARS = \
-	PATH=$(BS_PATH) \
+	PATH=$(PATH) \
 	ARCH=$(KERNEL_ARCH) \
 	CROSS_COMPILE=$(TARGET_CROSS) \
 	WERROR=0 \
@@ -77,6 +77,6 @@ endef
 define LINUX_RUN_DEPMOD
 	@$(call MESSAGE,"Running depmod")
 	if test -d $(TARGET_MODULES_DIR) && grep -q "CONFIG_MODULES=y" $(KERNEL_OBJ_DIR)/.config; then \
-		PATH=$(BS_PATH):/sbin:/usr/sbin depmod -a -b $(TARGET_DIR) $(KERNEL_VERSION); \
+		PATH=$(PATH):/sbin:/usr/sbin depmod -a -b $(TARGET_DIR) $(KERNEL_VERSION); \
 	fi
 endef
