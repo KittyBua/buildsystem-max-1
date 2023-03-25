08b1e910d9eab670e99c71a089f3acaf17ea54a0
max_10 <max_10@gmx.de>
2023-03-25 15:54:06 +0100

    - libevent: bump version to 2.1.12-stable

--- package/target/libevent/libevent.mk
+++ package/target/libevent/libevent.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-LIBEVENT_VERSION = 2.1.11-stable
+LIBEVENT_VERSION = 2.1.12-stable
 LIBEVENT_DIR = libevent-$(LIBEVENT_VERSION)
 LIBEVENT_SOURCE = libevent-$(LIBEVENT_VERSION).tar.gz
 LIBEVENT_SITE = https://github.com/libevent/libevent/releases/download/release-$(LIBEVENT_VERSION)
+
+LIBEVENT_DEPENDS = openssl
 
 define LIBEVENT_TARGET_CLEANUP
 	rm -f $(addprefix $(TARGET_BIN_DIR)/,event_rpcgen.py)
