a189df077a3417f405f5e32e254a3513960bc78d
max_10 <max_10@gmx.de>
2023-03-25 15:52:12 +0100

    - libdvdcss: bump version to 1.4.3

--- package/target/libdvdcss/libdvdcss.mk
+++ package/target/libdvdcss/libdvdcss.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDVDCSS_VERSION = 1.4.2
+LIBDVDCSS_VERSION = 1.4.3
 LIBDVDCSS_DIR = libdvdcss-$(LIBDVDCSS_VERSION)
 LIBDVDCSS_SOURCE = libdvdcss-$(LIBDVDCSS_VERSION).tar.bz2
 LIBDVDCSS_SITE = https://download.videolan.org/pub/libdvdcss/$(LIBDVDCSS_VERSION)
