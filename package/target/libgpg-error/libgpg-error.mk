4ecd9cdfec57f4f6f14975914c59fd04270922fb
max_10 <max_10@gmx.de>
2023-03-25 15:55:33 +0100

    - libgpg-error: bump version to 1.46

--- package/target/libgpg-error/libgpg-error.mk
+++ package/target/libgpg-error/libgpg-error.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBGPG_ERROR_VERSION = 1.42
+LIBGPG_ERROR_VERSION = 1.46
 LIBGPG_ERROR_DIR = libgpg-error-$(LIBGPG_ERROR_VERSION)
 LIBGPG_ERROR_SOURCE = libgpg-error-$(LIBGPG_ERROR_VERSION).tar.bz2
 LIBGPG_ERROR_SITE = https://www.gnupg.org/ftp/gcrypt/libgpg-error
 
-LIBGPG_ERROR_AUTORECONF = YES
-
-LIBGPG_ERROR_CONFIG_SCRIPTS = gpg-error-config
+#LIBGPG_ERROR_AUTORECONF = YES
 
 LIBGPG_ERROR_CONF_OPTS = \
 	--localedir=$(REMOVE_localedir) \
@@ -23,6 +21,7 @@ LIBGPG_ERROR_CONF_OPTS = \
 
 define LIBGPG_ERROR_TARGET_CLEANUP
 	rm -f $(addprefix $(TARGET_BIN_DIR)/,gpg-error gpgrt-config yat2m)
+	rm -rf $(addprefix $(TARGET_SHARE_DIR)/,common-lisp libgpg-error)
 endef
 LIBGPG_ERROR_TARGET_CLEANUP_HOOKS += LIBGPG_ERROR_TARGET_CLEANUP
 
