6bc1d0e2faf72f12cb832df42b29b33e8a3343eb
max_10 <max_10@gmx.de>
2023-03-25 15:56:59 +0100

    - procps-ng: bump version to 4.0.3

--- package/target/procps-ng/procps-ng.mk
+++ package/target/procps-ng/procps-ng.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROCPS_NG_VERSION = 4.0.0
+PROCPS_NG_VERSION = 4.0.3
 PROCPS_NG_DIR = procps-ng-$(PROCPS_NG_VERSION)
 PROCPS_NG_SOURCE = procps-ng-$(PROCPS_NG_VERSION).tar.xz
 PROCPS_NG_SITE = http://sourceforge.net/projects/procps-ng/files/Production
