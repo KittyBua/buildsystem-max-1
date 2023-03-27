e23f32ec15bf968cae83e3ccf74621893bb0e654
max_10 <max_10@gmx.de>
2023-03-27 21:47:16 +0200

    - crosstool-ng: bump version to 89671bf2

--- package/toolchain/crosstool-ng/crosstool-ng.mk
+++ package/toolchain/crosstool-ng/crosstool-ng.mk
@@ -14,7 +14,7 @@ CROSSTOOL_NG_DEPENDS = directories kernel.do_prepare
 CROSSTOOL_NG_CONFIG = crosstool-ng-$(TARGET_ARCH)-$(CROSSTOOL_GCC_VERSION)
 CROSSTOOL_NG_BACKUP = $(DL_DIR)/$(CROSSTOOL_NG_CONFIG)-kernel-$(KERNEL_VERSION)-backup.tar.gz
 
-CROSSTOOL_NG_CHECKOUT = 9e3c574e
+CROSSTOOL_NG_CHECKOUT = 89671bf2
 
 ifeq ($(wildcard $(CROSS_DIR)/build.log.bz2),)
 CROSSTOOL = crosstool
