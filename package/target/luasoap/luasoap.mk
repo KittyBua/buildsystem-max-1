6634a7845aad944b9a15246f9be99bac5c870dd4
max_10 <max_10@gmx.de>
2023-04-28 21:29:50 +0200

    - luasoap: fix url

--- package/target/luasoap/luasoap.mk
+++ package/target/luasoap/luasoap.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LUASOAP_VERSION = 3.0
+LUASOAP_VERSION = 3_0
 LUASOAP_DIR = luasoap-$(LUASOAP_VERSION)
 LUASOAP_SOURCE = luasoap-$(LUASOAP_VERSION).tar.gz
-LUASOAP_SITE = https://github.com/downloads/tomasguisasola/luasoap
+LUASOAP_SITE = $(call github,tomasguisasola,luasoap,v$(LUASOAP_VERSION))
 
 LUASOAP_DEPENDS = lua luasocket luaexpat
 
