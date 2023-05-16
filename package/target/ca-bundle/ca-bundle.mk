7e8f19f918e972fec6a1383040f1a8ef30d0242b
max_10 <max_10@gmx.de>
2023-05-16 11:13:03 +0200

    - ca-bundle: update build

--- package/target/ca-bundle/ca-bundle.mk
+++ package/target/ca-bundle/ca-bundle.mk
@@ -8,17 +8,16 @@ CA_BUNDLE_VERSION = 1.0
 CA_BUNDLE_DIR = /etc/ssl/certs
 CA_BUNDLE_SOURCE = cacert.pem
 CA_BUNDLE_SITE = https://curl.se/ca
+CA_BUNDLE_SITE_METHOD = curl
 
-CA_BUNDLE_CRT = ca-certificates.crt
+CA_BUNDLE_CERT = ca-certificates.crt
 
-define CA_BUNDLE_INSTALL_FILES
-	$(INSTALL_DATA) -D $(DL_DIR)/$(CA_BUNDLE_SOURCE) $(TARGET_DIR)/$(CA_BUNDLE_DIR)/$(CA_BUNDLE_CRT)
+define CA_BUNDLE_INSTALL
+	$(INSTALL_DATA) -D $(DL_DIR)/$($(PKG)_SOURCE) $(TARGET_DIR)/$(CA_BUNDLE_DIR)/$(CA_BUNDLE_CERT)
 endef
-CA_BUNDLE_POST_FOLLOWUP_HOOKS += CA_BUNDLE_INSTALL_FILES
+CA_BUNDLE_POST_FOLLOWUP_HOOKS += CA_BUNDLE_INSTALL
 
 $(D)/ca-bundle: | bootstrap
 	$(call STARTUP)
 	$(call DOWNLOAD,$($(PKG)_SOURCE))
-	$(CD) $(DL_DIR); \
-		curl --silent --remote-name --remote-time -z $(CA_BUNDLE_SOURCE) $(CA_BUNDLE_SITE)/$(CA_BUNDLE_SOURCE) || true
 	$(call TARGET_FOLLOWUP)
