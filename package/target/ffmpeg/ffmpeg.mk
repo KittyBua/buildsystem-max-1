685737a2b8eaa51f5b9bf2b0e801260499290d1b
max_10 <max_10@gmx.de>
2023-05-17 19:43:53 +0200

    - ffmpeg: bump version to 4.4.4

--- package/target/ffmpeg/ffmpeg.mk
+++ package/target/ffmpeg/ffmpeg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FFMPEG_VERSION ?= 4.4.1
+FFMPEG_VERSION ?= 4.4.4
 FFMPEG_DIR = ffmpeg-$(FFMPEG_VERSION)
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.xz
 FFMPEG_SITE = http://www.ffmpeg.org/releases
