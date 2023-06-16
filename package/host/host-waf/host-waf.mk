0e8c0c4c0feba565d10d5128a090e72cf3c2cf78
max_10 <max_10@gmx.de>
2023-06-16 22:36:55 +0200

    - host-waf: add depends

--- package/host/host-waf/host-waf.mk
+++ package/host/host-waf/host-waf.mk
@@ -9,6 +9,8 @@ WAF_DIR = waf-$(WAF_VERSION)
 WAF_SOURCE = waf-$(WAF_VERSION)
 WAF_SITE = https://waf.io
 
+HOST_WAF_DEPENDS = host-python3
+
 HOST_WAF_BINARY = $(HOST_DIR)/bin/waf
 
 define HOST_WAF_INSTALL
