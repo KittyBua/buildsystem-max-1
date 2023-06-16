8a913295c6214fda4f156497f131d70ca7e2f160
max_10 <max_10@gmx.de>
2023-06-16 22:34:53 +0200

    - pkg-meson: rework build

--- package/host/host-meson/host-meson.mk
+++ package/host/host-meson/host-meson.mk
@@ -11,7 +11,5 @@ MESON_SITE = https://github.com/mesonbuild/meson/releases/download/$(MESON_VERSI
 
 HOST_MESON_DEPENDS = host-ninja host-python3 host-python3-setuptools
 
-HOST_MESON_BINARY = $(HOST_DIR)/bin/meson
-
 $(D)/host-meson: | bootstrap
 	$(call host-python3-package)
--- package/host/host-ninja/host-ninja.mk
+++ package/host/host-ninja/host-ninja.mk
@@ -9,7 +9,5 @@ NINJA_DIR = ninja-$(NINJA_VERSION)
 NINJA_SOURCE = ninja-$(NINJA_VERSION).tar.gz
 NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
 
-HOST_NINJA_BINARY = $(HOST_DIR)/bin/ninja
-
 $(D)/host-ninja: | bootstrap
 	$(call host-cmake-package)
