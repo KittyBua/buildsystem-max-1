8db1a567df56d4e95af4486805580d2fcec00adf
max_10 <max_10@gmx.de>
2023-08-14 22:25:23 +0200

    - host-pkgconf: bump to version 2.0.1

--- package/host/host-pkgconf/host-pkgconf.mk
+++ package/host/host-pkgconf/host-pkgconf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PKGCONF_VERSION = 1.9.5
+PKGCONF_VERSION = 2.0.1
 PKGCONF_DIR = pkgconf-$(PKGCONF_VERSION)
 PKGCONF_SOURCE = pkgconf-$(PKGCONF_VERSION).tar.xz
 PKGCONF_SITE = https://distfiles.ariadne.space/pkgconf
