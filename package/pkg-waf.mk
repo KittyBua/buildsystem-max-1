f0d19243c98d72791c8dcf41c70c09ba19fc2b49
max_10 <max_10@gmx.de>
2023-09-25 13:44:23 +0200

    - pkg-waf: adjustment of the target package infrastructure

--- package/pkg-waf.mk
+++ package/pkg-waf.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-WAF_OPTS = $(if $(VERBOSE),-v) -j $(PARALLEL_JOBS)
+TARGET_WAF_OPTS = $(if $(VERBOSE),-v) -j $(PARALLEL_JOBS)
 
-WAF_CONFIGURE_OPTIONS = \
+TARGET_WAF_CONFIGURE_OPTIONS = \
 	--target=$(GNU_TARGET_NAME) \
 	--prefix=$(prefix) \
 	--exec-prefix=$(exec_prefix) \
@@ -19,38 +19,38 @@ WAF_CONFIGURE_OPTIONS = \
 	--sysconfdir=$(sysconfdir) \
 	$($(PKG)_CONF_OPTS)
 
-define WAF_CONFIGURE_CMDS_DEFAULT
-	$(CD) $($(PKG)_BUILD_DIR)/$($(PKG)_SUBDIR); \
+define TARGET_WAF_CONFIGURE_CMDS_DEFAULT
+	$(CD) $(PKG_BUILD_DIR)/$($(PKG)_SUBDIR); \
 	$(TARGET_CONFIGURE_ENV) \
 	$($(PKG)_CONF_ENV) \
 	$(HOST_PYTHON3_BINARY) $(HOST_WAF_BINARY) \
-		configure $(WAF_CONFIGURE_OPTIONS) \
+		configure $(TARGET_WAF_CONFIGURE_OPTIONS) \
 		$($(PKG)_CONF_OPTS) \
 		$($(PKG)_WAF_OPTS)
 endef
 
-define WAF_CONFIGURE
+define TARGET_WAF_CONFIGURE
 	@$(call MESSAGE,"Configuring")
 	$(foreach hook,$($(PKG)_PRE_BUILD_HOOKS),$(call $(hook))$(sep))
-	$(Q)$(call WAF_CONFIGURE_CMDS_DEFAULT)
+	$(Q)$(call TARGET_WAF_CONFIGURE_CMDS_DEFAULT)
 	$(foreach hook,$($(PKG)_POST_BUILD_HOOKS),$(call $(hook))$(sep))
 endef
 
-define WAF_BUILD_CMDS_DEFAULT
+define TARGET_WAF_BUILD_CMDS_DEFAULT
 	$(CD) $(PKG_BUILD_DIR)/$($(PKG)_SUBDIR); \
 	$(TARGET_MAKE_ENV) $($(PKG)_MAKE_ENV) $(HOST_PYTHON3_BINARY) $(HOST_WAF_BINARY) \
-		build $(WAF_OPTS) $($(PKG)_BUILD_OPTS) \
+		build $(TARGET_WAF_OPTS) $($(PKG)_BUILD_OPTS) \
 		$($(PKG)_WAF_OPTS)
 endef
 
-define WAF_BUILD
+define TARGET_WAF_BUILD
 	@$(call MESSAGE,"Building")
 	$(foreach hook,$($(PKG)_PRE_BUILD_HOOKS),$(call $(hook))$(sep))
 	$(Q)$(call $(PKG)_BUILD_CMDS)
 	$(foreach hook,$($(PKG)_POST_BUILD_HOOKS),$(call $(hook))$(sep))
 endef
 
-define WAF_INSTALL_CMDS_DEFAULT
+define TARGET_WAF_INSTALL_CMDS_DEFAULT
 	$(CD) $(PKG_BUILD_DIR)/$($(PKG)_SUBDIR); \
 	$(TARGET_MAKE_ENV) $($(PKG)_MAKE_ENV) $(HOST_PYTHON3_BINARY) $(HOST_WAF_BINARY) \
 		install --destdir=$(TARGET_DIR) \
@@ -58,7 +58,7 @@ define WAF_INSTALL_CMDS_DEFAULT
 		$($(PKG)_WAF_OPTS)
 endef
 
-define WAF_INSTALL
+define TARGET_WAF_INSTALL
 	@$(call MESSAGE,"Installing")
 	$(foreach hook,$($(PKG)_PRE_INSTALL_HOOKS),$(call $(hook))$(sep))
 	$(Q)$(call $(PKG)_INSTALL_CMDS)
@@ -68,8 +68,8 @@ endef
 define waf-package
 	$(eval PKG_MODE = $(pkg-mode))
 	$(call PREPARE,$(1))
-	$(call WAF_CONFIGURE)
-	$(if $(filter $(1),$(PKG_NO_BUILD)),,$(call WAF_BUILD))
-	$(if $(filter $(1),$(PKG_NO_INSTALL)),,$(call WAF_INSTALL))
+	$(call TARGET_WAF_CONFIGURE)
+	$(if $(filter $(1),$(PKG_NO_BUILD)),,$(call TARGET_WAF_BUILD))
+	$(if $(filter $(1),$(PKG_NO_INSTALL)),,$(call TARGET_WAF_INSTALL))
 	$(call HOST_FOLLOWUP)
 endef
