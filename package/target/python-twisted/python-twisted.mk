3f600f3a5bed6568db533f63bb9f61032c553043
max_10 <max_10@gmx.de>
2023-09-28 19:58:52 +0200

    - python-twisted: bump to version 22.10.0

--- package/target/python-twisted/python-twisted.mk
+++ package/target/python-twisted/python-twisted.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_TWISTED_VERSION = 18.4.0
+PYTHON_TWISTED_VERSION = 22.10.0
 PYTHON_TWISTED_DIR = Twisted-$(PYTHON_TWISTED_VERSION)
-PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.bz2
-PYTHON_TWISTED_SITE = https://pypi.python.org/packages/source/T/Twisted
+PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.gz
+PYTHON_TWISTED_SITE = https://files.pythonhosted.org/packages/b2/ce/cbb56597127b1d51905b0cddcc3f314cc769769efc5e9a8a67f4617f7bca
 
 PYTHON_TWISTED_DEPENDS = python-zope-interface python-constantly python-incremental python-pyopenssl python-service-identity
 
