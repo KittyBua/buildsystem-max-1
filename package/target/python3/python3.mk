60c53143d5bba0f82e1a2f2951a5783d75759242
max_10 <max_10@gmx.de>
2023-09-28 20:01:32 +0200

    - python: bump to version 3.11.5

--- package/target/python3/python3.mk
+++ package/target/python3/python3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON3_VERSION ?= 3.8.2
+PYTHON3_VERSION = 3.11.5
 PYTHON3_DIR = Python-$(PYTHON3_VERSION)
 PYTHON3_SOURCE = Python-$(PYTHON3_VERSION).tar.xz
 PYTHON3_SITE = https://www.python.org/ftp/python/$(PYTHON3_VERSION)
@@ -15,10 +15,17 @@ PYTHON3_DEPENDS = host-python3 ncurses zlib openssl libffi expat bzip2
 PYTHON3_LIB_DIR = usr/lib/python$(basename $(PYTHON3_VERSION))
 PYTHON3_INCLUDE_DIR = usr/include/python$(basename $(PYTHON3_VERSION))
 PYTHON3_SITE_PACKAGES_DIR = $(PYTHON3_LIB_DIR)/site-packages
+PYTHON3_PATH = $(TARGET_LIB_DIR)/python$(basename $(PYTHON3_VERSION))/
 
 PYTHON3_AUTORECONF = YES
 
 PYTHON3_CONF_ENV = \
+	ac_sys_system=Linux \
+	ac_sys_release=2 \
+	MACHDEP=linux \
+	ac_cv_have_chflags=no \
+	ac_cv_have_lchflags=no \
+	ac_cv_broken_sem_getvalue=no \
 	ac_cv_have_long_long_format=yes \
 	ac_cv_file__dev_ptmx=yes \
 	ac_cv_file__dev_ptc=no \
@@ -27,19 +34,18 @@ PYTHON3_CONF_ENV = \
 
 PYTHON3_CONF_OPTS = \
 	--enable-shared \
+	--enable-optimizations \
 	--without-ensurepip \
 	--without-cxx-main \
 	--with-build-python=$(HOST_PYTHON3_BINARY) \
 	--with-system-ffi \
 	--disable-pydoc \
-	--disable-ipv6 \
 	--disable-test-modules \
 	--disable-tk \
 	--disable-nis \
 	--disable-idle3 \
 	--disable-lib2to3 \
-	--disable-berkeleydb \
-	--disable-readline
+	--disable-berkeleydb
 
 $(D)/python3: | bootstrap
 	$(call autotools-package)
