675e5740a4ba91938cdb345aec060a13571f4a6b
max_10 <max_10@gmx.de>
2023-09-28 19:58:12 +0200

    - python-setuptools: bump to version 68.2.0

--- package/target/python-setuptools/python-setuptools.mk
+++ package/target/python-setuptools/python-setuptools.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_VERSION = 44.0.0
+PYTHON_SETUPTOOLS_VERSION = 68.2.0
 PYTHON_SETUPTOOLS_DIR = setuptools-$(PYTHON_SETUPTOOLS_VERSION)
-PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).zip
-PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/b0/f3/44da7482ac6da3f36f68e253cb04de37365b3dba9036a3c70773b778b485
+PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SITE = $(call github,pypa,setuptools,v$(PYTHON_SETUPTOOLS_VERSION))
 
 $(D)/python-setuptools: | bootstrap
 	$(call python-package)
